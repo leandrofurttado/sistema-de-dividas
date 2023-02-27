373498cfc6afb293eb88b6e1f5f57fe436082430
Leandro Souza <leandro.souza@southti.local>
2023-02-27 17:13:15 -0300

    Att

--- adicionar_divida.php
+++ adicionar_divida.php
@@ -5,14 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar dívidas</title>
+    <link rel="stylesheet" href="css/style_divida.css">
 </head>
 <body>
     <form action="">
-        
-        <input type="text" name="description" maxlength="100">
-        <input type="text" name="">
-        <input type="text" name="">
+        <input type="text" name="origem" maxlength="100" placeholder="Origem">
+        <input type="text" name="description" maxlength="100" placeholder="Descrição">
+        <input type="text" name="valor" maxlength="20" placeholder="Valor">
+        <input type="date" name="vencimento">
         <input type="submit" name="cadastrar">
     </form>
+
+    <h3>Adicionar dívidas:</h3>
+    <div class="caixa-maior">
+        <a href="">
+            <div class="caixa">+</div>
+        </a>
+        <div class="items"></div>
+    </div>
 </body>
 </html>
